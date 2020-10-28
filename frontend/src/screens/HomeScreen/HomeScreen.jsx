9287b49323ec136bb58696f035acf4a355602df9
helping083 <helping083@gmail.com>
2020-10-28 17:51:37 +0300

    develop init

---
+++ frontend/src/screens/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import PropTypes from 'prop-types'
+import products from '../../products';
+
+const HomeScreen = props => {
+  return (
+    <>
+      <h1>products works</h1>
+      <Row></Row>
+    </>
+  )
+}
+
+HomeScreen.propTypes = {
+
+}
+
+export default HomeScreen
