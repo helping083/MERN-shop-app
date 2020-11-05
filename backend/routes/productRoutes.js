e95ec34994618f5e71d580e9f1c3dcd98f6ae3a6
helping083 <helping083@gmail.com>
2020-11-06 01:11:19 +0200

    product logic to the controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,28 +1,17 @@
 const express = require('express');
-const Product = require('../models/productModel');
-const asyncHandler = require('express-async-handler');
 const router = express.Router();
+const {getProducts, getProductById} =
+  require('../controllers/product.controller');
 
 // @desc    Fetch all products
 // @route   GET /api/products
 // @access  Public
-router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
-}));
+router.route('/').get(getProducts);
 
 
 // @desc    Fetch single product
 // @route   GET /api/products/:id
 // @access  Public
-router.get('/:id', asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404);
-    throw new Error('product not found');
-  }
-}));
+router.route('/:id').get(getProductById);
 
 module.exports = router;
