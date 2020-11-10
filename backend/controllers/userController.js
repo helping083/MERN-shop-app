50b9d69aac3e9a8adebf9a2e6f8eb034dd0a59b6
helping083 <helping083@gmail.com>
2020-11-10 21:19:35 +0200

    auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,3 +22,21 @@ exports.authUser = asyncHandler(async (req, res) => {
     throw new Error('invalid email or password');
   }
 });
+
+// @desc    Get user profile
+// @route   Post /api/users/profile
+// @access  Private
+exports.getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('invalid email or password');
+  }
+});
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {authUser} = require('../controllers/userController');
+const {protect} = require('../middlewares/authMiddleware');
+const {authUser, getUserProfile} = require('../controllers/userController');
 
 router.post('/login', authUser);
+router.route('/profile').get(protect, getUserProfile);
 
 module.exports = router;
