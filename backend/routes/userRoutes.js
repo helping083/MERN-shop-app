e31ed90d0936b0939c1fe32c78b1039f97103e6a
helping083 <helping083@gmail.com>
2020-11-11 18:33:12 +0200

    hashing password and register a user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {protect} = require('../middlewares/authMiddleware');
-const {authUser, getUserProfile} = require('../controllers/userController');
+const {
+  authUser,
+  getUserProfile,
+  registerUser} = require('../controllers/userController');
 
+router.route('/').post(registerUser);
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile);
 
