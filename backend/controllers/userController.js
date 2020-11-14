0f36bfce6a96dd1a5b8c788ae9673fdf3cedd47b
helping083 <helping083@gmail.com>
2020-11-14 20:38:53 +0200

    update user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,3 +74,29 @@ exports.getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('invalid email or password');
   }
 });
+
+// @desc    Update user profile
+// @route   Put /api/users/profile
+// @access  Private
+exports.updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+  console.log(req.body);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('invalid email or password');
+  }
+});
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,14 @@ const {protect} = require('../middlewares/authMiddleware');
 const {
   authUser,
   getUserProfile,
+  updateUserProfile,
   registerUser} = require('../controllers/userController');
 
 router.route('/').post(registerUser);
 router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile);
+router
+    .route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
 
 module.exports = router;
